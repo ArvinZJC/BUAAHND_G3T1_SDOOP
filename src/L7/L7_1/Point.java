ea602822bf7e90d526a9151a49ae1137299bcc15
ArvinZJC <tomzjc@qq.com>
2018-10-31 21:30:38 +0800

    add L7 project

---
+++ src/L7/L7_1/Point.java
@@ -0,0 +1,19 @@
+//2018.10.31, class that gets and sets a point
+
+package L7.L7_1;
+
+//a generic class
+public class Point<T extends Number>
+{
+	private T x, y;
+	
+	Point( T x, T y )
+	{
+		this.x = x;
+		this.y = y;
+	} //end constructor Point
+	
+	public T getX() { return x; } //end method getX
+	
+	public T getY() { return y; } //end method getY
+} //end class Point
